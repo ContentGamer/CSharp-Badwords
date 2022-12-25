468a4d05e9142adf2f836a32df7bc6ad5467bb69
ContentGamer <85808595+ContentGamer@users.noreply.github.com>
2022-12-25 15:33:32 +0100

    Initial commit

---
+++ Filter.cs
@@ -0,0 +1,50 @@
+﻿namespace BadWordFilter;
+
+public class Filter
+{
+    private string profanityWord = "*";
+    private string[] contents = new BadWords()._wordList;
+
+    public Filter(string placeHolder = "*") {
+        this.profanityWord = placeHolder;
+    }
+
+    public string clean(string word) {
+        string newWord = word.ToLower();
+        foreach (string content in contents) {
+            if(word.ToLower() == content) {
+                newWord = word.Replace(content, content.Replace(content, profanityWord));
+            }
+        }
+        return newWord;
+    }
+
+    public bool isProfanity(string word) {
+        bool result = true;
+        foreach (string content in contents) {
+            if(word.ToLower() == content) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public void addWords(string[] words) {
+        foreach(string word in words) {
+            new BadWords()._wordList.Append(word);
+            contents = new BadWords()._wordList;
+        }
+    }
+
+    public void removeWords(string[] words) {
+        foreach(string word in words) {
+            for(var i = 0; i < new BadWords()._wordList.Length; i++){
+                if(new BadWords()._wordList[i] == word) {
+                    new BadWords()._wordList[i] = "";
+                    contents = new BadWords()._wordList;
+                }
+            }
+        }
+    }
+
+}
